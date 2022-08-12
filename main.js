b8a34d7883ebdac7ab04f0f94f7bb10963e8d381
Ju Dantas <jucienyds@gmail.com>
2022-08-11 21:05:09 -0300

    finaliza projeto

--- main.js
+++ main.js
@@ -1,1 +1,78 @@
-console.log("oi!")
+const readline = require("readline-sync")
+
+const menu = `
+====== MAIN MENU ======
+
+a > Add person
+l > List people
+q > Quit
+
+Type a option:
+`
+
+const listMenu = `
+====== LIST PEOPLE BY... ======
+
+a > Age
+n > Name
+q > Quit
+
+Type a option:
+`
+
+const people = []
+
+const getCategory = (age) => {
+    if (age >= 0 && age <= 12) {
+        return "Criança"
+    }
+    if (age >= 13 && age <= 19) {
+        return "Adolescente"
+    }
+    if (age >= 20 && age <= 64) {
+        return "Adulto"
+    }
+
+    return "Idoso"
+}
+
+console.log(menu)
+readline.promptCLLoop({
+    a: () => {
+        const name = readline.question("Name: ")
+        const age = readline.question("Age: ")
+        const category = getCategory(age)
+        people.push({ name, age, category })
+        console.log(menu)
+    },
+    l: () => {
+        console.log(listMenu)
+        readline.promptCLLoop({
+            a: () => {
+                const sorted = people.sort(
+                    (personA, personB) => personA.age - personB.age
+                )
+                console.table(sorted)
+                console.log(listMenu)
+            },
+            n: () => {
+                const sorted = people.sort(
+                    (a, b) => {
+                        if (a.name > b.name) {
+                            return 1;
+                        }
+                        if (a.name < b.name) {
+                            return -1;
+                        }
+                        return 0;
+                    }
+                )
+                console.table(sorted)
+                console.log(listMenu)
+            },
+            q: () => true
+        })
+        console.log(menu)
+    },
+    q: () => true
+})
